400863f17724e126b750384eec2321834c2feaf0
Matthew Yee <myee@redhat.com>
2026-02-23 13:01:18 -0800

    Add configuration and setup scripts for new RHEL3 instance
    - Updated `ui-config.yml` to include the new "rhel3.lab terminal" entry.
    - Enhanced `instances.yaml` with detailed configuration for the RHEL3 virtual machine.
    - Added setup scripts for RHEL3 in both `runtime-automation` and `setup-automation` directories to facilitate initial setup and package management adjustments.

---
+++ runtime-automation/01-introduction/setup-rhel3.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+curl -k -O /root/katello-server-ca.crt https://satellite.lab/pub/katello-server-ca.crt
+podman run --publish 8080:8080 --volume /root/katello-server-ca.crt:/app/ca.pem:ro,Z   registry.redhat.io/satellite/foreman-mcp-server-rhel9:6.18 --foreman-url https://satellite.lab --no-verify-ssl &
