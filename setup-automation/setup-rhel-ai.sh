c2dc0bb5f1e6a26e629f442641f20e7e611fa69e
Matthew Yee <yee.matthew@gmail.com>
2026-04-02 13:28:13 -0700

    Add configuration for OpenCode and update FOREMAN_TOKEN handling in setup script

--- setup-automation/setup-rhel-ai.sh
+++ setup-automation/setup-rhel-ai.sh
@@ -65,8 +65,9 @@ done && SATELLITE_UP=true
 
 if $SATELLITE_UP; then
   # SSH to Satellite, create admin Hammer access token for MCP.
-  export FOREMAN_TOKEN=$(ssh satellite.lab 'hammer user access-token create --user=admin --name="mcp server"')
+  FOREMAN_TOKEN=$(ssh satellite.lab 'hammer user access-token create --user=admin --name="mcp server"')
+  echo "$FOREMAN_TOKEN" > /tmp/FOREMAN_TOKEN
 
   # Inject FOREMAN_TOKEN into the OpenCode config.
-  sed -i "s/\"FOREMAN_TOKEN\": \"\"/\"FOREMAN_TOKEN\": \"$FOREMAN_TOKEN\"/" /root/.config/opencode/config.json
+  sed -i "s/\"FOREMAN_TOKEN\": \"\"/\"FOREMAN_TOKEN\": \"$(cat /tmp/FOREMAN_TOKEN)\"/" /root/.config/opencode/config.json
 fi
