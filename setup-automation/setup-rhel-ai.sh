ddd861249fbd91bcc9925b6957d148a3fb04ad28
Matthew Yee <yee.matthew@gmail.com>
2026-04-02 14:12:18 -0700

    Update FOREMAN_TOKEN creation command to specify token name more clearly

--- setup-automation/setup-rhel-ai.sh
+++ setup-automation/setup-rhel-ai.sh
@@ -65,7 +65,7 @@ done && SATELLITE_UP=true
 
 if $SATELLITE_UP; then
   # SSH to Satellite, create admin Hammer access token for MCP.
-  FOREMAN_TOKEN=$(ssh satellite.lab 'hammer user access-token create --user=admin --name="mcp server"')
+  FOREMAN_TOKEN=$(ssh satellite.lab 'hammer user access-token create --user=admin --name="mcp server token"')
   echo "$FOREMAN_TOKEN" > /tmp/FOREMAN_TOKEN
 
   # Inject FOREMAN_TOKEN into the OpenCode config.
